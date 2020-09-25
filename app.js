44904c38b0c3b8416d748b8bbefa20c9b8ddf5ca
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-25 17:45:53 -0300

    Versão 0.5, Finalizando a template da pagina de registrar

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.get('/', (req, res) => {
   res.render("index")
 });
 
+app.get('/registrar', (req, res) => {
+  res.render("registrar")
+});
+
 app.set('view engine', 'hbs');
 
 const diretorioPublico = path.join(__dirname,'./public')
