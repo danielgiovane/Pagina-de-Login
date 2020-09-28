b265c038d0bb47d9643551308176912862590263
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-28 15:28:59 -0300

    Versão 0.8, Usando bcrypt para hashear as senhas, validando campos e inserindo no mysql

--- controller/autenticacao.js
+++ controller/autenticacao.js
@@ -1,8 +1,48 @@
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USUARIO,
+  password: process.env.DATABASE_SENHA,
+  database: process.env.DATABASE
+});
+
+
 exports.registrar = (req, res) => {
-  if(req.body !== undefined){
-    res.send('Formulario submetido com sucesso')
-    console.log(req.body)
-  }else {
-    console.log('formulario undefined')
-  }
+  console.log(req.body)
+
+  const { nome, email, senha, confirmaSenha } = req.body;
+
+  db.query('SELECT email FROM usuarios WHERE email = ?', [email], async (error, resultado) => {
+    if (error) {
+      console.log(error);
+    }
+
+    if (resultado.length > 0) {
+      return res.render('registrar', {
+        message: 'Email em uso'
+      })
+    } else if (senha !== confirmaSenha) {
+      return res.render('registrar', {
+        message: 'Senha não confere'
+      });
+    }
+
+    const senhaComHash = await bcrypt.hash(senha, 8);
+    console.log(senhaComHash)
+
+    db.query('INSERT INTO usuarios SET ?', {nome: nome, email: email, senha: senhaComHash}, (error, resultado) => {
+      if(error){
+        console.log(error)
+      }else {
+        console.log(resultado)
+        return res.render('registrar', {
+          message: 'Registrado com suceeso'
+        });
+      }
+    })
+  })
+
 }
